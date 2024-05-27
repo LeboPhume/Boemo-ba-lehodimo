bc502d62aad6ae29ef2e304c0df1599359e92e27
LeboPhume <tyabashe.phume@gmail.com>
2024-05-27 12:20:53 +0200

    reworked index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,98 @@
 function refreshWeather(response) {
-  let temperatureElement = document.querySelector("#temperature");
-  let temperature = response.data.temperature.current;
-  let cityElement = document.querySelector("#city");
-  console.log(response.data.temperature.current);
+  let elementTemperature = document.querySelector("#temperature");
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let cityName = document.querySelector("#city");
+  let elementDiscription = document.querySelector("#weather-description");
+  let currentWeatherCondition = response.data.condition.description;
+  let elementHumidity = document.querySelector("#humidity");
+  let currentHumidity = `${response.data.temperature.humidity}%`;
+  let elementWind = document.querySelector("#wind-speed");
+  let currentWindSpeed = `${response.data.wind.speed}km/h`;
+  let timeElement = document.querySelector("#date");
+  let date = new Date(response.data.time * 1000);
+  let icon = document.querySelector("#icon");
+  let iconImage = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" id="weather-app-icon" />`;
 
-  cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = Math.round(temperature);
+  cityName.innerHTML = response.data.city;
+  elementTemperature.innerHTML = currentTemperature;
+  elementDiscription.innerHTML = currentWeatherCondition;
+  elementHumidity.innerHTML = currentHumidity;
+  elementWind.innerHTML = currentWindSpeed;
+  timeElement.innerHTML = formatDate(date);
+  icon.innerHTML = iconImage;
+  getForecast(response.data.city);
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
-  let apiKey = "c55ftfcaa714e3e000aee3a34e77cbo8";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+  let apikey = "b318adc2a4e42ff5a70at1cf59b30fbo";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    searchCity(searchInput.value);
-  }
-  let searchFormElement = document.querySelector("#search-form");
-  if (searchFormElement) {
-    searchFormElement.addEventListener("submit", handleSearchSubmit);
-  } else {
-    console.error('Element with id "search-form" not found.');
-  }
-});
+function handleSearch(event) {
+  event.preventDefault();
+  let searchedCityName = document.querySelector("#search-form-input");
+  searchCity(searchedCityName.value);
+}
 
-searchCity("Cape Town");
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apikey = "c55ftfcaa714e3e000aee3a34e77cbo8";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric";
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+  <div class="weather-forecast-days">
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+    <div class="weather-forecast-temperatures">
+      <div class="weather-forecast-temperature">
+        <strong>${Math.round(day.temperature.maximum)}°</strong>
+      </div>
+      <div class="weather-forecast-temperature">
+      ${Math.round(day.temperature.minimum)}°</div>
+    </div>
+  </div>
+`;
+    }
+  });
+
+  let forecast = document.querySelector("#forecast");
+  forecast.innerHTML = forecastHtml;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearch);
+searchCity("Pretoria");
